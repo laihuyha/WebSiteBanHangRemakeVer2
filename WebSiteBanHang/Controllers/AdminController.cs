2935eec3cb74f417fd47b0d4976ce0c8163481d1
Lại Huy Hà <laihuyha@gmail.com>
2021-07-26 16:30:41 +0700

    Add project files.

---
+++ WebSiteBanHang/Controllers/AdminController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+
+namespace WebSiteBanHang.Controllers
+{
+    [Authorize(Roles = "QuanTri")]
+    public class AdminController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: Admin
+        public ActionResult TrangChu()
+        {
+            ViewBag.TongDoanhThu = ThongKeDoanhThu();
+            ViewBag.TongDDH = ThongKeDonHang();
+            ViewBag.TongThanhVien = TongThanhVien();
+            ViewBag.TongLuotXem = TongLuotXem();
+            return View();
+        }
+
+        public ActionResult StatusPartial()
+        {
+            ViewBag.TongDoanhThu = ThongKeDoanhThu();
+            ViewBag.TongDDH = ThongKeDonHang();
+            ViewBag.TongThanhVien = TongThanhVien();
+            ViewBag.TongLuotXem = TongLuotXem();
+            return PartialView();
+        }
+
+        public decimal? ThongKeDoanhThu()
+        {
+            var TongDoanhThu = db.DonDatHangs.Where(n => n.DaThanhToan == true);
+            decimal? TongTien = 0;
+            //Duyệt chi tiết đơn hàng theo điều kiện
+            foreach (var item in TongDoanhThu)
+            {
+                TongTien += item.ChiTietDonDatHangs.Sum(n => n.DonGia * n.SoLuong);
+            }
+            return TongTien;
+        }
+
+        public double ThongKeDonHang()
+        {
+            //Đếm đơn đặt hàng
+            double sl = db.DonDatHangs.Count();
+            return sl;
+        }
+
+        public double TongThanhVien()
+        {
+            double slTV = db.ThanhViens.Count();
+            return slTV;
+        }
+
+        public double TongLuotXem()
+        {
+            var SanPham = db.SanPhams.Where(n => n.DaXoa == false);
+            int tongluotXem = 0;
+            foreach (var item in SanPham)
+            {
+                tongluotXem = (int)SanPham.Sum(o => o.LuotXem);
+            }
+            return tongluotXem;
+        }
+    }
+}
---
+++ WebSiteBanHang/Models/Metadata/ThanhVien.metadata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebSiteBanHang.Models
+{
+    [MetadataTypeAttribute(typeof(ThanhVienMetaData))]
+    public  partial class ThanhVien
+    {
+        internal sealed class ThanhVienMetaData
+        {
+
+            [Display(Name = "Mã Thành viên")]
+            public int MaThanhVien { get; set; }
+            [Display(Name = "Tài khoản")]
+            [Required(ErrorMessage = "Vui lòng nhập tên tài khoản")]
+            public string TaiKhoan { get; set; }
+            [Display(Name = "Mật khẩu")]
+            [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+            public string MatKhau { get; set; }
+            [Display(Name = "Họ và Tên")]
+            public string HoTen { get; set; }
+            [Display(Name = "Địa chỉ")]
+            public string DiaChi { get; set; }
+            [Display(Name = "Email")]
+            [EmailAddress(ErrorMessage = "Mail ảo à?")]
+            public string Email { get; set; }
+            [Display(Name = "Số điện thoại")]
+            public string SoDienThoai { get; set; }
+            [Display(Name = "Câu hỏi bảo mật")]
+            public string CauHoi { get; set; }
+            [Display(Name = "Câu trả lời")]
+            public string CauTraLoi { get; set; }
+            [Display(Name = "Mã loại thành viên")]
+            public Nullable<int> MaLoaiTV { get; set; }
+        }
+    }
+}
